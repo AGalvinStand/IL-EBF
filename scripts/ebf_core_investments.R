c5a9b0a5ff9186fa643f53949a8d944813d1def4
cpoulo2 <111312394+cpoulo2@users.noreply.github.com>
2022-08-15 15:09:10 -0500

    Core investments - in progress
    To be completed: additional investments, total costs, and adding integer calculations.

--- scripts/ebf_core_investments.R
+++ scripts/ebf_core_investments.R
@@ -1,6 +1,6 @@
 # PURPOSE ---------
 
-#   This script will perform the core investmetns calculation.
+#   This script will perform the core investments calculations.
 
 # -------------
 
@@ -8,11 +8,9 @@
 
 # Chris Poulos
 
-# 2022-08-09
+# 2022-08-10
 
 # Let's go! - <('.' <)
-
-#----------
 
 # load ---------
 
@@ -21,38 +19,36 @@ options(scipen = 999)
 library(tidyverse)
 library(readxl)
 
-# CORE INVESTMENTS - STEPS
-
-# Create ---------
-# Outline --------
-# Blah -----------
-
-source("scripts/ebf_enroll_li_clean.R")
-#source("scripts/ebf_enroll_all_clean.R")
-source("scripts/ebf_control_variables_clean.R")
+# CORE INVESTMENT STEPS --------
 
 # CORE TEACHER AMOUNT------
 
+# Bring in Low-income enrollment and control vairable dataframes.
 
-#Core teachers per student by grade
+source("scripts/ebf_enroll_li_clean.R")
+source("scripts/ebf_control_variables_clean.R")
 
-gk_3 <- il_fy22_cvs_core_class_size_clean$grade == "K-3"
-g4_12 <- il_fy22_cvs_core_class_size_clean$grade == "4-12"
+# Extract class size variables (by grade category and low-income status)
 
+li_k_3 <- as.numeric(il_fy22_cvs_core_class_size_clean$li[1])
+nonli_k_3 <- as.numeric(il_fy22_cvs_core_class_size_clean$nonli[1])
+
+li_4_12 <- as.numeric(il_fy22_cvs_core_class_size_clean$li[2])
+nonli_4_12 <- as.numeric(il_fy22_cvs_core_class_size_clean$nonli[2])
+
+# FTE core teachers to students by grade - taking into account income
 
 ebf_core_investments_fy22 <- il_fy22_enroll_li_clean |>
-  mutate(fte_coreteacher_k_3 = (enroll_k_3_li/il_fy22_cvs_core_class_size_clean$li[gk_3])+(enroll_k_3_noli/il_fy22_cvs_core_class_size_clean$nonli[gk_3]),
-         fte_coreteacher_4_8 = (enroll_4_8_li/il_fy22_cvs_core_class_size_clean$li[g4_12])+(enroll_4_8_noli/il_fy22_cvs_core_class_size_clean$nonli[g4_12]),
-         fte_coreteacher_9_12 = (enroll_9_12_li/il_fy22_cvs_core_class_size_clean$li[g4_12])+(enroll_9_12_noli/il_fy22_cvs_core_class_size_clean$nonli[g4_12]))
+  mutate(fte_coreteacher_k_3 = as.integer(((enroll_k_3_li/li_k_3)+(enroll_k_3_noli/nonli_k_3))*100)/100,
+         fte_coreteacher_4_8 = as.integer(((enroll_4_8_li/li_4_12)+(enroll_4_8_noli/nonli_4_12))*100)/100,
+         fte_coreteacher_9_12 = as.integer(((enroll_9_12_li/li_4_12)+(enroll_9_12_noli/nonli_4_12))*100)/100)
 
+# Core teacher salary by grade
 
-# Core teacher amount (salarly times fte core teachers per student)
+k8_coreteacher_salary <- as.numeric(il_fy22_cvs_avgsalary_clean$core_teacher[1])
+hs_coreteacher_salary <- as.numeric(il_fy22_cvs_avgsalary_clean$core_teacher[2])
 
-gk_8 = il_fy22_cvs_avgsalary_clean$grade == "Elementary (K - 8) (NO PK)"
-g9_12 = il_fy22_cvs_avgsalary_clean$grade == "High School (9 - 12)"
-
-k8_coreteacher_salary <- il_fy22_cvs_avgsalary_clean$core_teacher[gk_8]
-hs_coreteacher_salary <- il_fy22_cvs_avgsalary_clean$core_teacher[g9_12]
+# Core teacher cost
 
 ebf_core_investments_fy22 <- ebf_core_investments_fy22 |>
   mutate(core_teacher_cost = 
@@ -64,19 +60,19 @@ ebf_core_investments_fy22 <- ebf_core_investments_fy22 |>
 
 # Specialist Teacher % of Core Teacher
 
-gpk_5 <- il_fy22_cvs_core_investments_clean$grade == "Elementary (PK - 5)"
-gmiddle <- il_fy22_cvs_core_investments_clean$grade == "Middle"
-ghs <- il_fy22_cvs_core_investments_clean$grade == "High School"
+specialists_pk_5 <- il_fy22_cvs_core_investments_clean$specialists_per_of_coreteachers[1]
+specialists_middle <- il_fy22_cvs_core_investments_clean$specialists_per_of_coreteachers[2]
+specialists_hs <- il_fy22_cvs_core_investments_clean$specialists_per_of_coreteachers[3]
 
-specialists_pk_5 <- il_fy22_cvs_core_investments_clean$specialists_per_of_coreteachers[gpk_5]
-specialists_middle <- il_fy22_cvs_core_investments_clean$specialists_per_of_coreteachers[gmiddle]
-specialists_hs <- il_fy22_cvs_core_investments_clean$specialists_per_of_coreteachers[ghs]
+# Specialist teacher by grade
 
 ebf_core_investments_fy22 <- ebf_core_investments_fy22 |>
   mutate(fte_specialists = 
            (specialists_pk_5*fte_coreteacher_k_3)+
            (specialists_middle*fte_coreteacher_4_8)+
            (specialists_hs*fte_coreteacher_9_12))
+
+# Specialist teacher cost
 
 ebf_core_investments_fy22 <- ebf_core_investments_fy22 |>
   mutate(specialist_cost = 
@@ -88,12 +84,18 @@ ebf_core_investments_fy22 <- ebf_core_investments_fy22 |>
 
 source("scripts/ebf_enroll_all_clean.R")
 
-instructional_facilitator_pk_5 <- il_fy22_cvs_core_investments_clean$instructional_facilitators[gpk_5]
-instructional_facilitator_middle <- il_fy22_cvs_core_investments_clean$instructional_facilitators[gmiddle]
-instructional_facilitator_hs <- il_fy22_cvs_core_investments_clean$instructional_facilitators[ghs]
+# Students for every 1 instructional facilitator by grade variable
+
+instructional_facilitator_pk_5 <- il_fy22_cvs_core_investments_clean$instructional_facilitators[1]
+instructional_facilitator_middle <- il_fy22_cvs_core_investments_clean$instructional_facilitators[2]
+instructional_facilitator_hs <- il_fy22_cvs_core_investments_clean$instructional_facilitators[3]
+
+# Join enrollment all tab data (to calculation FTE instructional facilitator by grade)
 
 ebf_core_investments_fy22 <- ebf_core_investments_fy22 |>
   left_join(il_fy22_enroll_all_clean, by = c("distid" = "distid")) 
+
+# Instructional facilitator by grade
 
 ebf_core_investments_fy22 <- ebf_core_investments_fy22 |>
   mutate(fte_instructional_facilitator_pk_5 =
@@ -103,6 +105,8 @@ ebf_core_investments_fy22 <- ebf_core_investments_fy22 |>
          fte_instructional_facilitator_hs =
            (enroll_school_hs_9_12/instructional_facilitator_hs))
 
+# Instructional facilitator cost
+
 ebf_core_investments_fy22 <- ebf_core_investments_fy22 |>
   mutate(instructional_facilitator_cost = 
            (fte_instructional_facilitator_pk_5* k8_coreteacher_salary)+
@@ -111,9 +115,13 @@ ebf_core_investments_fy22 <- ebf_core_investments_fy22 |>
 
 # CORE INTERVENTION TEACHER (Tutor) COST ---------
 
-core_intervention_pk_5 <- il_fy22_cvs_core_investments_clean$core_intervention_teacher[gpk_5]
-core_intervention_middle <- il_fy22_cvs_core_investments_clean$core_intervention_teacher[gmiddle]
-core_intervention_hs <- il_fy22_cvs_core_investments_clean$core_intervention_teacher[ghs]
+# Students for every 1 core intervention teacher by grade variable
+
+core_intervention_pk_5 <- il_fy22_cvs_core_investments_clean$core_intervention_teacher[1]
+core_intervention_middle <- il_fy22_cvs_core_investments_clean$core_intervention_teacher[2]
+core_intervention_hs <- il_fy22_cvs_core_investments_clean$core_intervention_teacher[3]
+
+# Core intervention teacher by grade
 
 ebf_core_investments_fy22 <- ebf_core_investments_fy22 |>
   mutate(fte_core_intervention_pk_5 =
@@ -122,6 +130,8 @@ ebf_core_investments_fy22 <- ebf_core_investments_fy22 |>
            (enroll_school_mid_6_8/core_intervention_middle),
          fte_core_intervention_hs =
            (enroll_school_hs_9_12/core_intervention_hs))
+
+# Core intervention cost
 
 ebf_core_investments_fy22 <- ebf_core_investments_fy22 |>
   mutate(core_intervention_cost = 
@@ -135,12 +145,18 @@ ebf_core_investments_fy22 <- ebf_core_investments_fy22 |>
 
 # GUIDANCE COUNSELOR COST ------------------------
 
-guidance_counselor_pk_5 <- il_fy22_cvs_core_investments_clean$guidance_counselor[gpk_5]
-guidance_counselor_middle <- il_fy22_cvs_core_investments_clean$guidance_counselor[gmiddle]
-guidance_counselor_hs <- il_fy22_cvs_core_investments_clean$guidance_counselor[ghs]
+# Students for every 1 guidance counselor by grade variable
 
-k8_guidance_counselor_salary <- il_fy22_cvs_avgsalary_clean$guidance_counselor[gk_8]
-hs_guidance_counselor_salary <- il_fy22_cvs_avgsalary_clean$guidance_counselor[g9_12]
+guidance_counselor_pk_5 <- il_fy22_cvs_core_investments_clean$guidance_counselor[1]
+guidance_counselor_middle <- il_fy22_cvs_core_investments_clean$guidance_counselor[2]
+guidance_counselor_hs <- il_fy22_cvs_core_investments_clean$guidance_counselor[3]
+
+# Guidance counselor salary by grade variable
+
+k8_guidance_counselor_salary <- il_fy22_cvs_avgsalary_clean$guidance_counselor[1]
+hs_guidance_counselor_salary <- il_fy22_cvs_avgsalary_clean$guidance_counselor[2]
+
+# Guidance counselor by grade
 
 ebf_core_investments_fy22 <- ebf_core_investments_fy22 |>
   mutate(fte_guidance_counselor_pk_5 =
@@ -150,6 +166,8 @@ ebf_core_investments_fy22 <- ebf_core_investments_fy22 |>
          fte_guidance_counselor_hs =
            (enroll_school_hs_9_12/guidance_counselor_hs))
 
+# Guidance counselor cost
+
 ebf_core_investments_fy22 <- ebf_core_investments_fy22 |>
   mutate(guidance_counselor_cost = 
            (fte_guidance_counselor_pk_5* k8_guidance_counselor_salary)+
@@ -158,12 +176,18 @@ ebf_core_investments_fy22 <- ebf_core_investments_fy22 |>
 
 # NURSE COST -------------------------------------
 
-school_nurse_pk_5 <- il_fy22_cvs_core_investments_clean$nurse[gpk_5]
-school_nurse_middle <- il_fy22_cvs_core_investments_clean$nurse[gmiddle]
-school_nurse_hs <- il_fy22_cvs_core_investments_clean$nurse[ghs]
+# Students for every 1 nurse by grade variable
 
-k8_school_nurse_salary <- il_fy22_cvs_avgsalary_clean$school_nurse[gk_8]
-hs_school_nurse_salary <- il_fy22_cvs_avgsalary_clean$school_nurse[g9_12]
+school_nurse_pk_5 <- il_fy22_cvs_core_investments_clean$nurse[1]
+school_nurse_middle <- il_fy22_cvs_core_investments_clean$nurse[2]
+school_nurse_hs <- il_fy22_cvs_core_investments_clean$nurse[3]
+
+# Nurse salary by grade
+
+k8_school_nurse_salary <- il_fy22_cvs_avgsalary_clean$school_nurse[1]
+hs_school_nurse_salary <- il_fy22_cvs_avgsalary_clean$school_nurse[2]
+
+# Nurses per student by grade
 
 ebf_core_investments_fy22 <- ebf_core_investments_fy22 |>
   mutate(fte_school_nurse_pk_5 =
@@ -173,6 +197,8 @@ ebf_core_investments_fy22 <- ebf_core_investments_fy22 |>
          fte_school_nurse_hs =
            (enroll_school_hs_9_12/school_nurse_hs))
 
+# Nurse cost
+
 ebf_core_investments_fy22 <- ebf_core_investments_fy22 |>
   mutate(school_nurse_cost = 
            (fte_school_nurse_pk_5* k8_school_nurse_salary)+
@@ -181,12 +207,18 @@ ebf_core_investments_fy22 <- ebf_core_investments_fy22 |>
 
 # SUPERVISORY AIDE COST --------------------------
 
-supervisory_aide_pk_5 <- il_fy22_cvs_core_investments_clean$supervisory_aide[gpk_5]
-supervisory_aide_middle <- il_fy22_cvs_core_investments_clean$supervisory_aide[gmiddle]
-supervisory_aide_hs <- il_fy22_cvs_core_investments_clean$supervisory_aide[ghs]
+# Students for every 1 supervisory aide by grade variable
 
-k8_supervisory_aide_salary <- il_fy22_cvs_assumedsalary_clean$noninstruction_assistant[gk_8]
-hs_supervisory_aide_salary <- il_fy22_cvs_assumedsalary_clean$noninstruction_assistant[g9_12]
+supervisory_aide_pk_5 <- il_fy22_cvs_core_investments_clean$supervisory_aide[1]
+supervisory_aide_middle <- il_fy22_cvs_core_investments_clean$supervisory_aide[2]
+supervisory_aide_hs <- il_fy22_cvs_core_investments_clean$supervisory_aide[3]
+
+# Supervisory aide salary by grade
+
+k8_supervisory_aide_salary <- il_fy22_cvs_assumedsalary_clean$noninstruction_assistant[1]
+hs_supervisory_aide_salary <- il_fy22_cvs_assumedsalary_clean$noninstruction_assistant[2]
+
+# Supervisory aide by grade
 
 ebf_core_investments_fy22 <- ebf_core_investments_fy22 |>
   mutate(fte_supervisory_aide_pk_5 =
@@ -196,6 +228,8 @@ ebf_core_investments_fy22 <- ebf_core_investments_fy22 |>
          fte_supervisory_aide_hs =
            (enroll_school_hs_9_12/supervisory_aide_hs))
 
+# Supervisory aide cost
+
 ebf_core_investments_fy22 <- ebf_core_investments_fy22 |>
   mutate(supervisory_aide_cost = 
            (fte_supervisory_aide_pk_5* k8_supervisory_aide_salary)+
@@ -204,12 +238,18 @@ ebf_core_investments_fy22 <- ebf_core_investments_fy22 |>
 
 # LIBRARIAN COST ---------------------------------
 
-librarian_pk_5 <- il_fy22_cvs_core_investments_clean$librarian[gpk_5]
-librarian_middle <- il_fy22_cvs_core_investments_clean$librarian[gmiddle]
-librarian_hs <- il_fy22_cvs_core_investments_clean$librarian[ghs]
+# Students for every 1 librarian by grade variable
 
-k8_librarian_salary <- il_fy22_cvs_avgsalary_clean$librarian_mediaspecialist[gk_8]
-hs_librarian_salary <- il_fy22_cvs_avgsalary_clean$librarian_mediaspecialist[g9_12]
+librarian_pk_5 <- il_fy22_cvs_core_investments_clean$librarian[1]
+librarian_middle <- il_fy22_cvs_core_investments_clean$librarian[2]
+librarian_hs <- il_fy22_cvs_core_investments_clean$librarian[3]
+
+# Nurse salary by grade
+
+k8_librarian_salary <- il_fy22_cvs_avgsalary_clean$librarian_mediaspecialist[1]
+hs_librarian_salary <- il_fy22_cvs_avgsalary_clean$librarian_mediaspecialist[2]
+
+# Nurses per student
 
 ebf_core_investments_fy22 <- ebf_core_investments_fy22 |>
   mutate(fte_librarian_pk_5 =
@@ -219,6 +259,8 @@ ebf_core_investments_fy22 <- ebf_core_investments_fy22 |>
          fte_librarian_hs =
            (enroll_school_hs_9_12/librarian_hs))
 
+# Nurse cost
+
 ebf_core_investments_fy22 <- ebf_core_investments_fy22 |>
   mutate(librarian_cost = 
            (fte_librarian_pk_5* k8_librarian_salary)+
@@ -227,12 +269,18 @@ ebf_core_investments_fy22 <- ebf_core_investments_fy22 |>
 
 # LIBRARIAN AIDE / MEDIA TECH COST ---------------
 
-librarian_aide_pk_5 <- il_fy22_cvs_core_investments_clean$librarian_aide[gpk_5]
-librarian_aide_middle <- il_fy22_cvs_core_investments_clean$librarian_aide[gmiddle]
-librarian_aide_hs <- il_fy22_cvs_core_investments_clean$librarian_aide[ghs]
+# Students for every 1 librarian aide by grade variable
 
-k8_librarian_aide_salary <- il_fy22_cvs_assumedsalary_clean$noninstruction_assistant[gk_8]
-hs_librarian_aide_salary <- il_fy22_cvs_assumedsalary_clean$noninstruction_assistant[g9_12]
+librarian_aide_pk_5 <- il_fy22_cvs_core_investments_clean$librarian_aide[1]
+librarian_aide_middle <- il_fy22_cvs_core_investments_clean$librarian_aide[2]
+librarian_aide_hs <- il_fy22_cvs_core_investments_clean$librarian_aide[3]
+
+# Librarian aide salary by grade
+
+k8_librarian_aide_salary <- il_fy22_cvs_assumedsalary_clean$noninstruction_assistant[1]
+hs_librarian_aide_salary <- il_fy22_cvs_assumedsalary_clean$noninstruction_assistant[2]
+
+# Librarian aide per student 
 
 ebf_core_investments_fy22 <- ebf_core_investments_fy22 |>
   mutate(fte_librarian_aide_pk_5 =
@@ -242,6 +290,8 @@ ebf_core_investments_fy22 <- ebf_core_investments_fy22 |>
          fte_librarian_aide_hs =
            (enroll_school_hs_9_12/librarian_aide_hs))
 
+# Librarian aide cost
+
 ebf_core_investments_fy22 <- ebf_core_investments_fy22 |>
   mutate(librarian_aide_cost = 
            (fte_librarian_aide_pk_5* k8_librarian_aide_salary)+
@@ -250,12 +300,18 @@ ebf_core_investments_fy22 <- ebf_core_investments_fy22 |>
 
 # PRINCIPAL COST ---------------------------------
 
-principal_pk_5 <- il_fy22_cvs_core_investments_clean$principal[gpk_5]
-principal_middle <- il_fy22_cvs_core_investments_clean$principal[gmiddle]
-principal_hs <- il_fy22_cvs_core_investments_clean$principal[ghs]
+# Students for every 1 principal by grade variable
 
-k8_principal_salary <- il_fy22_cvs_avgsalary_clean$principal[gk_8]
-hs_principal_salary <- il_fy22_cvs_avgsalary_clean$principal[g9_12]
+principal_pk_5 <- il_fy22_cvs_core_investments_clean$principal[1]
+principal_middle <- il_fy22_cvs_core_investments_clean$principal[2]
+principal_hs <- il_fy22_cvs_core_investments_clean$principal[3]
+
+# Principal salary by grade
+
+k8_principal_salary <- il_fy22_cvs_avgsalary_clean$principal[1]
+hs_principal_salary <- il_fy22_cvs_avgsalary_clean$principal[2]
+
+# Principal per student
 
 ebf_core_investments_fy22 <- ebf_core_investments_fy22 |>
   mutate(fte_principal_pk_5 =
@@ -265,6 +321,8 @@ ebf_core_investments_fy22 <- ebf_core_investments_fy22 |>
          fte_principal_hs =
            (enroll_school_hs_9_12/principal_hs))
 
+# Principal cost
+
 ebf_core_investments_fy22 <- ebf_core_investments_fy22 |>
   mutate(principal_cost = 
            (fte_principal_pk_5* k8_principal_salary)+
@@ -273,12 +331,18 @@ ebf_core_investments_fy22 <- ebf_core_investments_fy22 |>
 
 # ASSISTANT PRINCIPAL COST -----------------------
 
-assistant_principal_pk_5 <- il_fy22_cvs_core_investments_clean$assistant_principal[gpk_5]
-assistant_principal_middle <- il_fy22_cvs_core_investments_clean$assistant_principal[gmiddle]
-assistant_principal_hs <- il_fy22_cvs_core_investments_clean$assistant_principal[ghs]
+# Students for every 1 Assistant principal by grade variable
 
-k8_assistant_principal_salary <- il_fy22_cvs_avgsalary_clean$assistant_principal[gk_8]
-hs_assistant_principal_salary <- il_fy22_cvs_avgsalary_clean$assistant_principal[g9_12]
+assistant_principal_pk_5 <- il_fy22_cvs_core_investments_clean$assistant_principal[1]
+assistant_principal_middle <- il_fy22_cvs_core_investments_clean$assistant_principal[2]
+assistant_principal_hs <- il_fy22_cvs_core_investments_clean$assistant_principal[3]
+
+# Assistant principal salary by grade
+
+k8_assistant_principal_salary <- il_fy22_cvs_avgsalary_clean$assistant_principal[1]
+hs_assistant_principal_salary <- il_fy22_cvs_avgsalary_clean$assistant_principal[2]
+
+# Assistant principal per student
 
 ebf_core_investments_fy22 <- ebf_core_investments_fy22 |>
   mutate(fte_assistant_principal_pk_5 =
@@ -288,6 +352,8 @@ ebf_core_investments_fy22 <- ebf_core_investments_fy22 |>
          fte_assistant_principal_hs =
            (enroll_school_hs_9_12/assistant_principal_hs))
 
+# Assistant principal cost
+
 ebf_core_investments_fy22 <- ebf_core_investments_fy22 |>
   mutate(assistant_principal_cost = 
            (fte_assistant_principal_pk_5* k8_assistant_principal_salary)+
@@ -296,12 +362,16 @@ ebf_core_investments_fy22 <- ebf_core_investments_fy22 |>
 
 # SCHOOL SITE STAFF COST -------------------------
 
-school_site_staff_pk_5 <- il_fy22_cvs_core_investments_clean$school_site_staff[gpk_5]
-school_site_staff_middle <- il_fy22_cvs_core_investments_clean$school_site_staff[gmiddle]
-school_site_staff_hs <- il_fy22_cvs_core_investments_clean$school_site_staff[ghs]
+# Students for every 1 School site staff by grade variable
 
-k8_school_site_staff_salary <- il_fy22_cvs_assumedsalary_clean$school_site_staff[gk_8]
-hs_school_site_staff_salary <- il_fy22_cvs_assumedsalary_clean$school_site_staff[g9_12]
+school_site_staff_pk_5 <- il_fy22_cvs_core_investments_clean$school_site_staff[1]
+school_site_staff_middle <- il_fy22_cvs_core_investments_clean$school_site_staff[2]
+school_site_staff_hs <- il_fy22_cvs_core_investments_clean$school_site_staff[3]
+
+k8_school_site_staff_salary <- il_fy22_cvs_assumedsalary_clean$school_site_staff[1]
+hs_school_site_staff_salary <- il_fy22_cvs_assumedsalary_clean$school_site_staff[2]
+
+# School site staff per student
 
 ebf_core_investments_fy22 <- ebf_core_investments_fy22 |>
   mutate(fte_school_site_staff_pk_5 =
@@ -310,6 +380,8 @@ ebf_core_investments_fy22 <- ebf_core_investments_fy22 |>
            (enroll_school_mid_6_8/school_site_staff_middle),
          fte_school_site_staff_hs =
            (enroll_school_hs_9_12/school_site_staff_hs))
+
+# School site staff cost
 
 ebf_core_investments_fy22 <- ebf_core_investments_fy22 |>
   mutate(school_site_staff_cost = 
